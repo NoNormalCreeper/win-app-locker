9b19ff90dcb857f873d027747e2c72c80197673c
NoNormalCreeper <2560359315@qq.com>
2022-04-30 14:12:41 +0800

    ⚡️ read config file

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+config_path = "../data/config.json"
+
+import json
+
+def read_config() -> dict:
+    with open(config_path) as f:
+        config = json.load(f)
+    return config
+
+def read_banlist() -> list:
+    config = read_config()
+    return config['banlist']
+
+def read_preference() -> dict:
+    config = read_config()
+    return config['preference']
