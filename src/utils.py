1f87e10bfc6b18b930c0a7bc911555aafd37870c
NoNormalCreeper <2560359315@qq.com>
2022-04-30 14:18:26 +0800

    ✨ exception handler

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,17 @@
 config_path = "./data/config.json"
 
+from doctest import Example
 import json
 
 def read_config() -> dict:
-    with open(config_path, encoding='utf-8') as f:
-        config = json.load(f)
+    try:
+        with open(config_path, encoding='utf-8') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        with open(config_path, 'w') as f:
+            with open("./data/default_config.json", encoding='utf-8') as df:
+                config = json.load(df)
+                json.dump(config, f)
     return config
 
 def read_banlist() -> list:
