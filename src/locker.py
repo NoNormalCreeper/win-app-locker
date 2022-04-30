de7318faeb110d8a4cd5a94e3c96f05aa76e4c7e
NoNormalCreeper <2560359315@qq.com>
2022-04-30 13:59:20 +0800

    💄 optimize returns

--- src/locker.py
+++ src/locker.py
@@ -11,13 +11,15 @@ def run_cmd(cmd: str) -> list:
     p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     return p.stdout.readlines()
 
-def kill_all_app(ban_list: list) -> bool:
+def kill_all_app(ban_list: list) -> list:
     """
     Kills all programs in the banlist
+    Returns a list of killed apps
     """
     cmd = f"taskkill /f /im {app}"
+    killed_apps = []
     for app in ban_list:
         if 'SUCCESS' in run_cmd(cmd):
             win32api.MessageBox(0, "别玩啦！", "WARNING",win32con.MB_ICONWARNING)
-            return True
-    return False
+            killed_apps.append(app)
+    return killed_apps if killed_apps else None
